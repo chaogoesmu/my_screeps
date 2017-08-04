8a052382473c31260456ab6a13564f8b5d504bce
chaogoesmu <chaogoesmu@gmail.com>
2017-08-03 21:56:24 -0700

    Update action.harvest.js
    no sub, need more efficient memory, now no longer calcing path repeatedly.

--- action.harvest.js
+++ action.harvest.js
@@ -2,10 +2,18 @@ var actHarvest = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        
-	var source = creep.pos.findClosestByPath(FIND_SOURCES);
+    if(creep.memory.MySource == undefined)
+    {
+        console.log(creep.name + ' Does not have a source yet, re-acquiring');
+	    creep.memory.MySource = creep.pos.findClosestByPath(FIND_SOURCES).id;
+    }
+    var source = Game.getObjectById(creep.memory.MySource)
     if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+        if(creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}})== ERR_NO_PATH){
+                creep.memory.MySource == undefined;
+                console.log(creep.name  + ' Unable to find path');
+                creep.memory.MySource = creep.pos.findClosestByPath(FIND_SOURCES).id;
+            }
         }
     }
 };
