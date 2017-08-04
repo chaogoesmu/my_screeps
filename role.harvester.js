11cbead3c7a28060ac93d4e544f4ea06038f6e6a
chaogoesmu <chaogoesmu@gmail.com>
2017-08-04 01:30:25 -0700

    Update role.harvester.js
    updated to use the actions option

--- role.harvester.js
+++ role.harvester.js
@@ -1,4 +1,7 @@
-var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+var actHarvest = require('action.harvest');
+var actDeposit = require('action.deposit');
+
 
 var roleHarvester = {
 
@@ -22,10 +25,10 @@ var roleHarvester = {
 		
 		switch(creep.memory.MyTask){
 			case 0://get more energy
-				forage(creep);
+				actHarvest.run(creep);
 			break;
 			case 1://go fill somethings energy
-    			deposit(creep);
+    			actDeposit.run(creep);
 			break;
 			default://uhoh
 			creep.memory.MyTask = 1;
@@ -33,43 +36,5 @@ var roleHarvester = {
 		}
 	}
 };
-function forage(creep)
-{
-	var source = creep.pos.findClosestByPath(FIND_SOURCES);
-    if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
-    }
-}
-function deposit(creep)
-{
-				var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-					filter: (structure) => {
-						return (structure.structureType == STRUCTURE_EXTENSION ||
-							structure.structureType == STRUCTURE_SPAWN ||
-							structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-						}
-					});
-				if(target!=undefined) {
-                    if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-                    }
-                }
-				else
-				{
-				    var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                        filter: (structure) => {
-                            return (structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_STORAGE) && (structure.store.energy < structure.storeCapacity );
-                            }
-                    });
-                    if(target != undefined) {
-                    if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                            creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-                    }
-                    }
-                    else{
-                        roleUpgrader.run(creep);
-                    }
-				}
-}
 
 module.exports = roleHarvester;
